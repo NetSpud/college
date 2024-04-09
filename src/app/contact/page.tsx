fcbea8a359d6e68f36a77a1747fca6e7eea8c463
NetSpud <56541762+NetSpud@users.noreply.github.com>
2024-04-09 10:58:27 +0100

    added student ID to form + loading spinner + page title + meta tags

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 function App() {
   return (
     <div>
@@ -10,7 +12,18 @@ function App() {
   );
 }
 
+function Spinner() {
+  return (
+    <svg className="animate-spin h-8 w-8 block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+  );
+}
+
 function Form() {
+  const [loading, setLoading] = useState(false);
+
   return (
     <form>
       <div>
@@ -22,11 +35,17 @@ function Form() {
         <input type="email" id="email" name="email" placeholder="Your email" className="w-full bg-gray-200 rounded-md p-2 focus:outline-none" />
       </div>
       <div>
-        <label htmlFor="message">Message</label>
-        <textarea id="message" name="message" placeholder="Your message" className="w-full bg-gray-200 rounded-md p-2 focus:outline-none"></textarea>
+        <label htmlFor="studentID">Student ID</label>
+        <input type="text" id="studentID" name="student_ID" placeholder="Your student ID" className="w-full bg-gray-200 rounded-md p-2 focus:outline-none" />
       </div>
-
-      <button className="px-3 py-2 rounded-md text-white font-semibold bg-ekc-b1 hover:bg-ekc-b2 duration-300">Submit</button>
+      <div>
+        <label htmlFor="message">Message</label>
+        <textarea id="message" rows={6} name="message" placeholder="Your message" className="w-full bg-gray-200 rounded-md p-2 focus:outline-none"></textarea>
+      </div>
+      <button type="button" onClick={() => setLoading(!loading)} className="px-3 py-2 rounded-md text-white flex gap-3 items-center font-semibold bg-ekc-b1 hover:bg-ekc-b2 duration-300">
+        {loading && <Spinner />}
+        <span className="inline-block">Send</span>
+      </button>
     </form>
   );
 }
