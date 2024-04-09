7dd8e50ffa439f1dd414390756c4134d5d9c698a
NetSpud <56541762+NetSpud@users.noreply.github.com>
2024-04-09 11:30:22 +0100

    Finished about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,12 +3,22 @@ import ContactBtn from "../components/contactBtn";
 
 export default function Page() {
   return (
-    <div className="pb-12">
-      <h1 className="text-3xl text-bold bg-gray-200 text-center p-3">ABOUT</h1>
-      <p></p>
-
-      <div className="text-center pt-12">
-        <ContactBtn />
+    <div>
+      <h1 className="text-4xl text-bold bg-ekc-b4 text-white font-bold text-center p-3 uppercase">About</h1>
+      <h2 className="text-2xl block text-center font-extrabold uppercase">Who is this for?</h2>
+      <p className="text-center text-lg p-3">This course is intended for Canterbury college students who want to learn the fundementals of JavaScript.</p>
+      <h2 className="text-2xl block text-center font-extrabold uppercase">What will you learn?</h2>
+      <p className="text-center text-lg p-3">You will learn the basics of JavaScript, including variables, loops, functions, and more.</p>
+      <h2 className="text-2xl block text-center font-extrabold uppercase">How will you learn?</h2>
+      <p className="text-center text-lg p-3">You will learn through a series of lessons, quizzes, and projects.</p>
+      <h2 className="text-2xl block text-center font-extrabold uppercase">What do you need?</h2>
+      <p className="text-center text-lg p-3">You will need a computer with internet access and a text editor like Visual Studio Code.</p>
+      <h2 className="text-2xl block text-center font-extrabold uppercase">How long will it take?</h2>
+      <p className="text-center text-lg p-3">This course will take approximately 2 hours to complete.</p>
+      <h2 className="text-2xl block text-center font-extrabold">Stuck?</h2>
+      <p className="text-center text-lg p-3">Have any issue or a question we haven&apos;t answered?</p>
+      <div className="mb-12 flex justify-center">
+        <ContactBtn text="Get in touch" />
       </div>
     </div>
   );
