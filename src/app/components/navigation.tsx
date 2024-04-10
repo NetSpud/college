784711105f85d6896ff2accd2c9e4e1a174b08e8
NetSpud <56541762+NetSpud@users.noreply.github.com>
2024-04-10 11:40:16 +0100

    added text to nav btn on mobile

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -39,10 +39,11 @@ const MobileNav = () => {
     <div className="sm:hidden">
       <div className="flex">
         <Logo />
-        <button onClick={() => setIsOpen(!isOpen)} className="px-3 py-2 ml-auto rounded-md bg-gray-500 hover:bg-gray-400 duration-300">
+        <button onClick={() => setIsOpen(!isOpen)} className="px-3 py-2 ml-auto rounded-md bg-gray-500 text-white hover:bg-gray-400 duration-300">
           <svg className="w-8 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
           </svg>
+          Open
         </button>
       </div>
       <ul className={`ml-8 flex-col items-center gap-5 ${isOpen ? "flex" : "hidden"}`}>
